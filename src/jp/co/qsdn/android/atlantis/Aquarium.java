b5fe67823dde0f346ab9fad838da02c101d517d4
Atsushi Konno <konn@users.sourceforge.jp>
2011-05-24 01:03:32 +0900

    *

---
+++ src/jp/co/qsdn/android/atlantis/Aquarium.java
@@ -0,0 +1,24 @@
+package jp.co.qsdn.android.atlantis;
+
+
+/**
+ * 水槽
+ */
+public class Aquarium {
+  // 水槽の大きさ（案）
+  // 10.0f >= x  >= -10.0f
+  // 8.0f >= y >= 0.0f
+  // -50.0f > z >= 0.0f
+  public static Float min_x = new Float(-10.0f);
+  public static Float max_x = new Float(10.0f);
+
+  public static Float min_y = new Float(0.2f);
+  public static Float max_y = new Float(8.0f);
+
+  public static Float min_z = new Float(-50.0f);
+  public static Float max_z = new Float(0.2f);
+
+  // 水槽の中心
+  public static float[] center = { 0.0f, 4.0f, -25f };
+
+}
